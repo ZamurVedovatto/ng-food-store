97a6405be21658474fc88b8e4b288725e0b2b5ea
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-02-12 19:53:29 -0300

    cart component

--- front/src/app/components/pages/cart-page/cart-page.component.ts
+++ front/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItem } from 'src/app/shared/models/CartItem';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.scss']
+  styleUrls: ['./cart-page.component.scss'],
 })
 export class CartPageComponent implements OnInit {
+  cart!: Cart;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private cartService: CartService) {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
   }
 
+  ngOnInit(): void {}
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantiy = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantiy);
+  }
 }
