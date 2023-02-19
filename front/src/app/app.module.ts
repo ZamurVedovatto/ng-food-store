c9e4856fb5db93a9b012058913a4edc34efcc161
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-02-19 08:36:59 -0300

    cleaning up deps

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { RatingModule } from 'ng-starrating';
+// import { RatingModule } from 'ng-starrating';
+// import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +21,7 @@ import { LoginPageComponent } from './components/pages/login-page/login-page.com
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent, CartPageComponent, TitleComponent, NotFoundComponent, LoginPageComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule, RatingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ front/src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+// import { ToastrService } from 'ngx-toastr';
+import { User } from './../shared/models/User';
+import { IUserLogin } from './../shared/interfaces/IUserLogin';
+import { USER_LOGIN_URL } from './../shared/constants/urls';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userSubject = new BehaviorSubject<User>(new User());
+  public userObservable: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.userObservable = this.userSubject.asObservable();
+  }
+
+  login(userLogin: IUserLogin): Observable<User> {
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {
+
+        },
+        error: (errorResponse) => {
+
+        }
+      })
+    )
+  }
+}
